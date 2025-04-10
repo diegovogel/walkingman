ec213b0789373f7ef3ae3a7feae0cd366d7d191c
Diego Vogel <diego@birdboar.co>
2025-04-10 15:10:39 -0400

    Create trips table

---
+++ database/migrations/2025_04_10_184810_create_trips_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+	public function up() {
+		Schema::create( 'trips', function ( Blueprint $table ) {
+			$table->id();
+            $table->foreignId('origin_city_id')->constrained('cities')->nullOnDelete();
+            $table->foreignId('destination_city_id')->nullable()->constrained('cities')->nullOnDelete();
+            $table->decimal('distance', 6);
+            $table->timestamp('departure');
+            $table->timestamp('arrival')->nullable();
+            $table->boolean('destination_from_user')->default(false); // Destination came from a user, whether chosen or selected randomly.
+            $table->boolean('destination_is_random')->default(true); // Destination selected randomly, whether by the app or a user.
+            $table->foreignId('user_id')->nullable()->constrained();
+			$table->timestamps();
+		} );
+	}
+
+	public function down() {
+		Schema::dropIfExists( 'trips' );
+	}
+};
