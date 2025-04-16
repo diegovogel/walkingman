b1c93cf315c182d30f33e2a3bd177da41283444d
Diego Vogel <diego@birdboar.co>
2025-04-16 17:11:28 -0400

    Add placeholder tests for GameSeeder with skip indicators

---
+++ tests/Feature/Seeders/GameSeederTest.php
@@ -0,0 +1,11 @@
+<?php
+
+it('should add a game to the database for each game file', function () {})->skip();
+
+it('should only create games from Markdown files', function () {})->skip();
+
+it('should log an error if no game files are found', function () {})->skip();
+
+it('should skip a game if there is a problem getting its info', function () {})->skip();
+
+it('should set the game status to "draft" if the given status is invalid or missing', function () {})->skip();
