2ab69b54a67c5d3e0964dc9f0261e5b83968dea0
Diego Vogel <diego@birdboar.co>
2026-08-17 18:54:42 -0400

    Widen the public content column to max-w-lg

--- resources/views/components/layouts/public.blade.php
+++ resources/views/components/layouts/public.blade.php
@@ -4,7 +4,7 @@
         @include('partials.head')
     </head>
     <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
-        <div class="mx-auto flex min-h-svh w-full max-w-md flex-col px-6 py-10">
+        <div class="mx-auto flex min-h-svh w-full max-w-lg flex-col px-6 py-10">
             {{ $slot }}
         </div>
         @fluxScripts
